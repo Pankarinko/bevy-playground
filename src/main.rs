95839199b2d79ebf3c8e88812043286da8462c63
Pankarinko <pankarinko@gmail.com>
2024-09-08 02:18:53 +0200

    Green square

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use bevy::{
+    color::palettes::css::{GREEN, YELLOW},
+    prelude::*,
+    sprite::MaterialMesh2dBundle,
+};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .insert_resource(ClearColor(YELLOW.into()))
+        .add_systems(Startup, spawn_rect)
+        .run();
+}
+
+fn spawn_rect(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: meshes.add(Rectangle::default()).into(),
+        material: materials.add(Color::from(GREEN)),
+        transform: Transform::default().with_scale(Vec3::splat(128.)),
+        ..default()
+    });
 }
